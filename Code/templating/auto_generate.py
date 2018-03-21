cbfdd1b7d4d3fd90617b9f120e44e810d06a20cd
Erin Dieringer <ediering@andrew.cmu.edu>
2018-03-20 22:03:01 -0400

    finalized

--- Code/templating/auto_generate.py
+++ Code/templating/auto_generate.py
@@ -3,6 +3,9 @@ import bs4
 from bs4 import BeautifulSoup
 from jinja2 import Environment, FileSystemLoader
 
+# User indicates the csv to be used in the upload
+csv_file = input("Enter csv filename to use: ")
+
 # User inputes whether to create img or modal
 templateType = input("img or modal?: ")
 
@@ -11,17 +14,17 @@ env = Environment(loader=FileSystemLoader(''))
 template = env.get_template( templateType + "Template.html" )
 
 # Local CSV to read data from
-rdr = csv.DictReader( open("test.csv", "r" , encoding='utf-8') )
+rdr = csv.DictReader( open(csv_file, "r" , encoding='utf-8') )
 
 # If image specified
 if templateType == "img":
 	
-	year = input("Enter semseter and yer: ")
+	year = input("Enter semseter and year: ")
 	year_string = year.replace(" ", "-")
 	img_data = []
 
 	# Open and instantiate file with beautiful soup 
-	with open("../projects2.html") as inf:
+	with open("../projects.html") as inf:
 	    txt = inf.read()
 	    soup = bs4.BeautifulSoup(txt, 'html.parser')
 	
@@ -31,7 +34,7 @@ if templateType == "img":
 			img_data.append(BeautifulSoup(img_content, 'html.parser'))
 	
 	section = f"""<h2>{year}</h2>
-    			<section class="row text-center placeholders" id="seventeen">
+    			<section class="row text-center placeholders" id="img-section">
 
     			</section>"""
 
@@ -39,11 +42,11 @@ if templateType == "img":
 	soup.find(class_="icon-images").insert(0, section_soup)
 
 	# Save the projects file
-	with open("../projects2.html", "w") as outf:
+	with open("../projects.html", "w") as outf:
 		outf.write(str(soup))
 	
 	# Reopen and instantiate file with beautiful soup 
-	with open("../projects2.html") as inf:
+	with open("../projects.html") as inf:
 	    txt = inf.read()
 	    soup = bs4.BeautifulSoup(txt, 'html.parser')
 	
@@ -52,25 +55,26 @@ if templateType == "img":
 		soup.find(class_="row text-center placeholders").insert(0, img)
 
 	# Save the projects file again
-	with open("../projects2.html", "w") as outf:
+	with open("../projects.html", "w") as outf:
 		outf.write(str(soup))
 
 # If modal specified
 elif templateType == "modal":
 	# Open and instantiate file with beautiful soup 
-	with open("../projects2.html") as inf:
+	with open("../projects.html") as inf:
 	    txt = inf.read()
 	    soup = bs4.BeautifulSoup(txt, 'html.parser')
 	
 	# Use template and csv data to render output
 	template_content = template.render( data=rdr )
 	template_soup = BeautifulSoup(template_content, 'html.parser')
-	old_modals = soup.find(class_="modals").findAll(class_="auto-generate")
 
 	# Insert into document
 	soup.find(class_="modals").insert(0,template_soup)
 
 
 	# Save the projects file
-	with open("../projects2.html", "w") as outf:
+	with open("../projects.html", "w") as outf:
 		outf.write(str(soup))
+
+
